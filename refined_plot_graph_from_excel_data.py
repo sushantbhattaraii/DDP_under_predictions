4579b6495c9e92925bd1320d344413feee489845
Sushant Bhattarai <sushantb2055@gmail.com>
2025-05-01 13:49:54 -0400

    plotted the final line graph

--- refined_plot_graph_from_excel_data.py
+++ refined_plot_graph_from_excel_data.py
@@ -8,7 +8,7 @@ from fractions import Fraction
 import os
 
 # Gather and sort all Excel files
-files = sorted(glob.glob('./results/error_and_stretch_data_with_cut-off_AND_overlap/1024/overlap50/1024nodes_diameter*.xlsx'))
+files = sorted(glob.glob('./results/error_and_stretch_data_with_cut-off_AND_overlap/64/overlap50/64nodes_diameter*.xlsx'))
 
 # Extract node count and overlap from filename
 m = re.search(r'(\d+)nodes_', files[0])
@@ -28,6 +28,10 @@ for idx, f in enumerate(files):
     df = pd.read_excel(f)
     mean_error = df.groupby('fraction')['error'].mean()
     mean_stretch = df.groupby('fraction')['stretch'].mean()
+    
+    mean_error.index = mean_error.index * int(node_count)
+    mean_stretch.index = mean_stretch.index * int(node_count)
+    
     all_x.extend(mean_error.index.tolist())
     
     # Extract cutoff value from filename
@@ -56,13 +60,14 @@ unique_x = sorted(set(all_x))
 
 # Format error subplot
 ax1.set_ylabel('Error')
+ax2.set_xlabel(f'Number of predicted nodes among {node_count} nodes (# of operations)')
 ax1.set_title(f'Error vs Fraction of Predicted Nodes (Overlap: {overlap_value}%)')
 ax1.legend(loc='best')
 # ax1.grid(True)
 
 # Format stretch subplot
 ax2.set_ylabel('Stretch')
-ax2.set_xlabel(f'Fraction of predicted nodes among {node_count} nodes (# of operations)')
+ax2.set_xlabel(f'Number of predicted nodes among {node_count} nodes (# of operations)')
 ax2.set_title(f'Stretch vs Fraction of Predicted Nodes (Overlap: {overlap_value}%)')
 ax2.legend(loc='best')
 # ax2.grid(True)
@@ -76,5 +81,5 @@ folder = "6_cutoffs_merged"
 filename = f'{node_count}_nodes_overlap{overlap_value}.png'
 path_to_save = os.path.join('results', folder, filename)
 
-plt.savefig(path_to_save)
+# plt.savefig(path_to_save)
 plt.show()
