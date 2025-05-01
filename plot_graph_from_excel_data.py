d2dedc8999b61243e6a3f7a03afeed74a83cee34
Sushant Bhattarai <sushantb2055@gmail.com>
2025-04-30 21:58:05 -0400

    made the plots cleaner and informative

--- plot_graph_from_excel_data.py
+++ plot_graph_from_excel_data.py
@@ -2,6 +2,9 @@ import pandas as pd
 import glob
 import matplotlib.pyplot as plt
 import re
+import numpy as np
+from matplotlib.ticker import MultipleLocator
+from fractions import Fraction
 
 
 # Gather and sort all 64-node Excel files
@@ -16,13 +19,20 @@ overlap_value = m2.group(1) if m else "?"
 # Prepare colormap
 cmap = plt.get_cmap('tab20')
 
-plt.figure(figsize=(8, 9))
+all_x = []
+
+plt.figure(figsize=(9, 10))
 for idx, f in enumerate(files):
     # Read data
     df = pd.read_excel(f)
     # Compute mean error and mean stretch by fraction
     mean_error = df.groupby('fraction')['error'].mean()
     mean_stretch = df.groupby('fraction')['stretch'].mean()
+    
+    all_x.extend(mean_error.index.tolist())
+    all_x.extend(mean_stretch.index.tolist())
+
+
     # Extract cutoff value from filename
     cutoff = f.split("_cutoff")[1].split("-")[0]
     actual_cutoff = 1/(float(cutoff))
@@ -43,10 +53,20 @@ for idx, f in enumerate(files):
         color=cmap(2 * idx + 1)
     )
 
+
+unique_x = sorted(set(all_x))
+
 # Labeling
 plt.xlabel(f'Fraction of predicted nodes among {node_count} nodes (# of operations)')
 plt.ylabel('Error / Stretch')
-plt.title(f'Error & stretch vs Fraction of nodes (# of operations) in {node_count} node graph | Overlap: {overlap_value}%')
+plt.title(f'Error/Stretch vs Fraction of nodes (# of operations) in {node_count} Node Graph | Prediction Overlap: {overlap_value}%')
+plt.grid(True)
+
+plt.vlines(x=mean_error.index, ymin=0, ymax=mean_error.values, color='gray', linestyle='--', linewidth=0.8)
+
+ax = plt.gca()
+ax.yaxis.set_major_locator(MultipleLocator(0.1))
+ax.set_xticks(unique_x)
 
 plt.legend(loc='best')
 plt.tight_layout()
